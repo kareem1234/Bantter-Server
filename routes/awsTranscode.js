7313df7b53a8ad04ae232c10d97ac9616fbe4947
kareem1234 <kareem.lewis79@gmail.com>
2014-10-27 18:52:58 -0400

    aws transcode api

--- routes/awsTranscode.js
+++ routes/awsTranscode.js
@@ -73,7 +73,7 @@ var params = {
   Output:{
         Key: newfileName,
         PresetId: preset,
-        ThumbnailPattern: imageName
+        ThumbnailPattern: "{count:00001}"
     }
   };
   console.dir(params);
